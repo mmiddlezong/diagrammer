69a7109e2cd06311256230c25da9ab2eab19f649
mmiddlezong <119022671+mmiddlezong@users.noreply.github.com>
2024-04-23 16:39:45 -0400

    Fix SVG size

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ type Point = {
     x: number;
     y: number;
 };
-const viewHeight = 15;
-const viewWidth = 20;
+const viewWidth = 32;
 const viewMaxX = viewWidth / 2;
+const viewHeight = 18;
 const viewMaxY = viewHeight / 2;
 const pointFillRadius = 0.06;
 function App() {
@@ -17,9 +17,12 @@ function App() {
 
     const [showAxes, setShowAxes] = useState<boolean>(true);
     const [showMajorGridLines, setShowMajorGridLines] = useState<boolean>(true);
-    const [showMinorGridLines, setShowMinorGridLines] = useState<boolean>(true);
+    const [showMinorGridLines, setShowMinorGridLines] = useState<boolean>(false);
     const [transform, setTransform] = useState<d3.ZoomTransform>(d3.zoomIdentity);
     const svgRef = useRef<SVGSVGElement>(null);
+
+    // const viewHeight = viewWidth * window.innerHeight / window.innerWidth;
+    // const viewMaxY = viewHeight / 2;
 
     useEffect(() => {
         if (svgRef.current) {
@@ -60,10 +63,28 @@ function App() {
         return (
             <>
                 {xValues.map((x) => {
-                    return <line x1={x} y1={-maxY} x2={x} y2={maxY} strokeWidth={0.02 / transform.k} className="stroke-gray-300" />;
+                    return (
+                        <line
+                            x1={x}
+                            y1={-maxY}
+                            x2={x}
+                            y2={maxY}
+                            strokeWidth={0.02 / transform.k}
+                            className="stroke-gray-300"
+                        />
+                    );
                 })}
                 {yValues.map((y) => {
-                    return <line x1={-maxX} y1={y} x2={maxX} y2={y} strokeWidth={0.02 / transform.k} className="stroke-gray-300" />;
+                    return (
+                        <line
+                            x1={-maxX}
+                            y1={y}
+                            x2={maxX}
+                            y2={y}
+                            strokeWidth={0.02 / transform.k}
+                            className="stroke-gray-300"
+                        />
+                    );
                 })}
             </>
         );
@@ -84,7 +105,16 @@ function App() {
         return (
             <>
                 {xValues.map((x) => {
-                    return <line x1={x} y1={-maxY} x2={x} y2={maxY} strokeWidth={0.01 / transform.k} className="stroke-gray-200" />;
+                    return (
+                        <line
+                            x1={x}
+                            y1={-maxY}
+                            x2={x}
+                            y2={maxY}
+                            strokeWidth={0.01 / transform.k}
+                            className="stroke-gray-200"
+                        />
+                    );
                 })}
                 {yValues.map((y) => {
                     return (
@@ -103,71 +133,78 @@ function App() {
     }
     return (
         <>
-            <div className="flex flex-row w-screen h-screen">
-                <svg
-                    ref={svgRef}
-                    viewBox={`${-viewMaxX} ${-viewMaxY} ${viewWidth} ${viewHeight}`}
-                    className="h-screen grow"
-                >
-                    <g transform={transform.toString()}>
-                        <g transform="scale(1,-1)">
-                            {showMinorGridLines && drawMinorGridLines()}
-                            {showMajorGridLines && drawMajorGridLines()}
-                            {showAxes && drawAxes()}
-                            {points.map((point, index) => {
-                                return (
-                                    <circle
-                                        key={index}
-                                        cx={point.x}
-                                        cy={point.y}
-                                        r={pointFillRadius / transform.k}
-                                        fill="black"
-                                    />
-                                );
-                            })}
-                        </g>
-                    </g>
-                </svg>
-                <div className="flex flex-col w-[250px] h-screen space-y-4 overflow-auto p-4 border-l shadow-md">
-                    <div className="flex items-center space-x-2">
-                        <input
-                            type="checkbox"
-                            id="showAxes"
-                            checked={showAxes}
-                            onChange={() => setShowAxes(!showAxes)}
-                            className="form-checkbox h-5 w-5 text-blue-600"
-                        />
-                        <label htmlFor="showAxes" className="text-lg text-gray-800">
-                            Show axes
-                        </label>
+            <div className="flex flex-col w-screen h-screen">
+                <div className="flex flex-row w-full grow">
+                    <div className="grow relative h-full">
+                        <svg
+                            ref={svgRef}
+                            viewBox={`${-viewMaxX} ${-viewMaxY} ${viewWidth} ${viewHeight}`}
+                            className="absolute left-0 top-0 w-full h-full"
+                            preserveAspectRatio="xMidYMid slice"
+                        >
+                            <g transform={transform.toString()}>
+                                <g transform="scale(1,-1)">
+                                    {showMinorGridLines && drawMinorGridLines()}
+                                    {showMajorGridLines && drawMajorGridLines()}
+                                    {showAxes && drawAxes()}
+                                    {points.map((point, index) => {
+                                        return (
+                                            <circle
+                                                key={index}
+                                                cx={point.x}
+                                                cy={point.y}
+                                                r={pointFillRadius / transform.k}
+                                                fill="black"
+                                            />
+                                        );
+                                    })}
+                                </g>
+                            </g>
+                        </svg>
                     </div>
-                    <div className="flex items-center space-x-2">
-                        <input
-                            type="checkbox"
-                            id="showMajorGridLines"
-                            checked={showMajorGridLines}
-                            onChange={() => setShowMajorGridLines(!showMajorGridLines)}
-                            className="form-checkbox h-5 w-5 text-blue-600"
-                        />
-                        <label htmlFor="showMajorGridLines" className="text-lg text-gray-800">
-                            Show major grid lines
-                        </label>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <input
-                            type="checkbox"
-                            id="showMinorGridLines"
-                            checked={showMinorGridLines}
-                            onChange={() => setShowMinorGridLines(!showMinorGridLines)}
-                            className="form-checkbox h-5 w-5 text-blue-600"
-                        />
-                        <label htmlFor="showMinorGridLines" className="text-lg text-gray-800">
-                            Show minor grid lines
-                        </label>
+                    <div className="flex flex-col w-64 h-full space-y-4 overflow-auto p-4 border-l border-blue-400">
+                        <div className="flex items-center space-x-2">
+                            <input
+                                type="checkbox"
+                                id="showAxes"
+                                checked={showAxes}
+                                onChange={() => setShowAxes(!showAxes)}
+                                className="form-checkbox h-5 w-5 text-blue-600"
+                            />
+                            <label htmlFor="showAxes" className="text-lg text-gray-800">
+                                Show axes
+                            </label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <input
+                                type="checkbox"
+                                id="showMajorGridLines"
+                                checked={showMajorGridLines}
+                                onChange={() => setShowMajorGridLines(!showMajorGridLines)}
+                                className="form-checkbox h-5 w-5 text-blue-600"
+                            />
+                            <label htmlFor="showMajorGridLines" className="text-lg text-gray-800">
+                                Show major grid lines
+                            </label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <input
+                                type="checkbox"
+                                id="showMinorGridLines"
+                                checked={showMinorGridLines}
+                                onChange={() => setShowMinorGridLines(!showMinorGridLines)}
+                                className="form-checkbox h-5 w-5 text-blue-600"
+                            />
+                            <label htmlFor="showMinorGridLines" className="text-lg text-gray-800">
+                                Show minor grid lines
+                            </label>
+                        </div>
                     </div>
                 </div>
+                <div className="border-t border-blue-400 h-6">
+                    <input autoFocus={true} type="text" className="w-full px-2 py-1 text-sm font-mono" />
+                </div>
             </div>
-            <input type="text" className="w-full"/>
         </>
     );
 }
